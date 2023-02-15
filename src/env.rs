16cfb4f8e5bbdc89da107aff844e60ce09e9f74e
Sullivan Muse <sullivan.muse@gmail.com>
2023-02-15 09:21:49 -0600

    Implement environment push and pop

--- src/env.rs
+++ src/env.rs
@@ -1,4 +1,5 @@
 use std::borrow::Borrow;
+use vec1::Vec1;
 
 pub(crate) trait Env<K, V>: Clone {
     fn insert(&mut self, key: K, value: V);
@@ -10,20 +11,22 @@ pub(crate) trait Env<K, V>: Clone {
     where
         Q: ?Sized,
         K: Borrow<Q> + PartialEq<Q>;
+    fn push(&mut self);
+    fn pop(&mut self);
 }
 
 #[derive(Clone, Debug, PartialEq)]
-pub(crate) struct EnvVec<K, V>(Vec<(K, V)>);
+pub(crate) struct EnvVec<K, V>(Vec1<Vec<(K, V)>>);
 
 impl<K, V> EnvVec<K, V> {
     pub(crate) fn new() -> Self {
-        Self(Vec::new())
+        Self(Vec1::new(Vec::new()))
     }
 }
 
 impl<K: Clone, V: Clone> Env<K, V> for EnvVec<K, V> {
     fn insert(&mut self, key: K, value: V) {
-        self.0.push((key, value));
+        self.0.last_mut().push((key, value));
     }
 
     fn get<Q>(&self, key: &Q) -> &V
@@ -34,8 +37,7 @@ impl<K: Clone, V: Clone> Env<K, V> for EnvVec<K, V> {
         self.0
             .iter()
             .rev()
-            .find(|(k, _)| k == key)
-            .map(|(_, v)| v)
+            .find_map(|v| v.iter().rev().find(|(k, _)| k == key).map(|(_, v)| v))
             .expect("Attempt to access undeclared variable.")
     }
 
@@ -47,9 +49,16 @@ impl<K: Clone, V: Clone> Env<K, V> for EnvVec<K, V> {
         self.0
             .iter_mut()
             .rev()
-            .find(|(k, _)| k == key)
-            .map(|(_, v)| v)
+            .find_map(|v| v.iter_mut().rev().find(|(k, _)| k == key).map(|(_, v)| v))
             .expect("Attempt to access undeclared variable.")
+    }
+
+    fn push(&mut self) {
+        self.0.push(Vec::new())
+    }
+
+    fn pop(&mut self) {
+        self.0.pop().expect("More pops than pushes.");
     }
 }
 
@@ -77,5 +86,17 @@ mod test {
         env.insert("x".to_string(), 1);
         *env.get_mut("x") = 2;
         assert_eq!(*env.get("x"), 2);
+    }
+
+    #[test]
+    fn test_push_pop() {
+        let mut env = EnvVec::new();
+        env.insert("x".to_string(), 1);
+        env.push();
+        env.insert("x".to_string(), 3);
+        *env.get_mut("x") = 2;
+        assert_eq!(*env.get("x"), 2);
+        env.pop();
+        assert_eq!(*env.get("x"), 1);
     }
 }
