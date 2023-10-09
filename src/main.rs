a449d39eea9e6fcfb30643c252b2387c9846f3c1
Sullivan Muse <sullivan.muse@gmail.com>
2023-10-09 11:25:01 -0500

    Implement command line interface

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,20 @@ mod eval;
 mod expr;
 mod parse;
 mod span;
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+use std::fs::read_to_string;
+use std::io::Write;
 
 use crate::{
     eval::{Intrinsics, Value},
     parse::expr,
 };
 
-fn main() {
-    fn input() -> String {
+fn repl() {
+    fn input(prompt: &str) -> String {
+        print!("{}", prompt);
+        std::io::stdout().flush().unwrap();
         let mut s = String::new();
         std::io::stdin().read_line(&mut s).unwrap();
         s
@@ -20,14 +26,54 @@ fn main() {
         let dec = |x: &Value| Value::Int(x.get_i64() - 1);
         let inc = |x: &Value| Value::Int(x.get_i64() + 1);
         let intrinsics: Intrinsics<'_> = vec![("dec", dec), ("inc", inc)];
-        let s = input();
+        let s = input("(fast) ");
         let span = s.as_str().into();
         match expr(span) {
             Ok((_, e)) => {
                 let value = e.eval_with_intrinsics(&intrinsics);
                 println!("{value:?}");
             }
-            _ => (),
+            _ => {}
         }
+    }
+}
+
+#[derive(Debug, Parser)]
+#[clap(name = "fast", version = "0.0.1", author = "J. Sullivan Muse")]
+struct Args {
+    #[command(subcommand)]
+    sub: Sub,
+}
+
+/// fast run FILE
+/// fast cmd 5*3
+/// fast repl
+#[derive(Debug, Subcommand)]
+enum Sub {
+    Run { input: PathBuf },
+    Cmd { segments: Vec<String> },
+    Repl,
+}
+
+fn main() {
+    let args = Args::parse();
+    let s = match args.sub {
+        Sub::Run { input } => read_to_string(input).unwrap(),
+        Sub::Cmd { segments } => segments.join(" "),
+        Sub::Repl => {
+            repl();
+            return;
+        }
+    };
+    let dec = |x: &Value| Value::Int(x.get_i64() - 1);
+    let inc = |x: &Value| Value::Int(x.get_i64() + 1);
+    let intrinsics: Intrinsics<'_> = vec![("dec", dec), ("inc", inc)];
+    let span = s.as_str().into();
+    match expr(span) {
+        Ok((_, e)) => {
+            let value = e.eval_with_intrinsics(&intrinsics);
+            println!("{value:?}");
+        }
+        _ => {}
     }
 }
